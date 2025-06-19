eaedf7798ce29587c8ea27c3d1b0653b2bfe5310
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-06-19 10:57:21 -0500

    is-prime check in divisors

--- divisors.rkt
+++ divisors.rkt
@@ -1,8 +1,16 @@
 #lang sicp
+
+(define (is-prime n)
+  (= n (smallest-divisor n)))
+
 (define (smallest-divisor n)
   (find-smallest-divisor n 2))
 
 (define (find-smallest-divisor target test_val)
-  (cond ((= target test_val) target)
+  (cond ((> (* test_val test_val) target) target)
+        ((= target test_val) target)
         ((= 0 (remainder target test_val)) test_val)
         (else (find-smallest-divisor target (+ test_val 1)))))
+
+(is-prime 4)
+(is-prime 5)
