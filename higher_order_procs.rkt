59e4cfa42d95f00ab1d3e6c20588f6df4faa2951
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-06-20 17:39:43 -0500

    generalize next step as well as function on term

--- higher_order_procs.rkt
+++ higher_order_procs.rkt
@@ -1,18 +1,20 @@
 #lang sicp
-(define (sum-thing a b proc)
-  (if (> a b) 0 (+ (proc a) (sum-thing (+ a 1) b proc))))
+(define (sum-thing a b proc next)
+  (if (> a b) 0 (+ (proc a) (sum-thing (next a) b proc))))
 
 (define (sum-integers a b)
-  (sum-thing a b (lambda (x) x)))
+  (sum-thing a b (lambda (x) x) inc)
+)
 
 (define (sum-cubes a b)
-  (sum-thing a b cube))
+  (sum-thing a b cube inc))
 
 (define (pi-sum a b)
-  (sum-thing a b (lambda (x) (/ 1.0 (* x (+ x 2))))))
+  (sum-thing a b (lambda (x) (/ 1.0 (* x (+ x 2))))
+                 (lambda (x) (+ x 4))))
 
 
-
+(define (inc n) (+ n 1))
 (define (cube n) (* n n n))
 
 (sum-integers 5 9)
